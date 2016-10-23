9b0969a620b135a00b1216962370949f58da821e
Niel C <nielchah@gmail.com>
2016-10-23 19:28:52 -0400

    Implemented saving as CSV dump

--- python/youtube-api.py
+++ python/youtube-api.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
-from collections import OrderedDict
 import csv
 import datetime
 import requests
+import time
 
 """
 Running:
@@ -15,6 +15,145 @@ $ python3 youtube-api.py [input_file OR video_id]
 # Globals: API key
 api_key = open('.api_key').read().strip()
 quota_counter = 100000
+
+# Starting up the CSV
+current_timestamp = str(datetime.datetime.now().strftime('%Y-%m-%d-%Hh-%Mm'))  # was .strftime('%Y-%m-%d'))
+csv_file_name = 'data/output/' + current_timestamp + '-youtube-comments.csv'
+headers = ['video_id', 'comment_id', 'comment_date', 'updated_date', 'commenter_name', 'top_level_comment',
+           'comment_reply']
+with open(csv_file_name, 'a') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow(headers)
+
+
+def timestamp_to_utc(timestamp):
+    """Convert unix timestamp to UTC date
+    :param timestamp: int - the unix timestamp integer
+    :return: utc_data - the date in YYYY-MM-DD format
+    """
+    timestamp = int(str(timestamp)[0:10])
+    utc_date = datetime.datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d')
+    return utc_date
+
+
+def store_csv(video_id, comments):
+    """Store the traffic stats as a CSV, with schema:
+    > Output Schema: [date_time]-comments.csv
+    video_id, comment_id, comment_date, updated_date, commenter_name, parent_comment, child_comment,
+
+    :param video_id: string -
+    :param comments: list -
+    """
+    for comment in comments:
+        row = [video_id,
+               comment['id'],
+               comment['snippet']['topLevelComment']['snippet']['publishedAt'],
+               comment['snippet']['topLevelComment']['snippet']['updatedAt'],
+               comment['snippet']['topLevelComment']['snippet']['authorDisplayName'],
+               comment['snippet']['topLevelComment']['snippet']['textDisplay'],
+               " - "]
+        with open(csv_file_name, 'a') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow(row)
+
+        # Getting the replies to each topLevelComment
+        if comment.get('replies'):
+            for reply in comment['replies']['comments']:
+                row = [video_id,
+                       reply['id'],
+                       reply['snippet']['publishedAt'],
+                       reply['snippet']['updatedAt'],
+                       reply['snippet']['authorDisplayName'],
+                       " - ",
+                       reply['snippet']['textDisplay']]
+
+                with open(csv_file_name, 'a') as csv_file:
+                    csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                    csv_writer.writerow(row)
+
+    return ''
+
+
+def send_request(resource, query_volume, video_id, part, max_results, order_by='relevance'):
+    """
+    :param resource: 'commentThreads' - only this for now
+    :param query_volume: 'all' or 'once' - set to 'all' to get all comments
+    :param video_id:
+    :param part:
+    :param max_results:
+    :param order_by:
+    :return:
+    """
+    if resource == 'commentThreads':
+        # GET https://www.googleapis.com/youtube/v3/commentThreads?
+        # ^ from https://developers.google.com/youtube/v3/guides/implementation/comments
+        base_url = 'https://www.googleapis.com/youtube/v3/commentThreads'
+        payload = {
+            'part': part,
+            'maxResults': max_results,
+            'order': order_by,
+            'videoId': video_id,
+            'key': api_key
+        }
+        response = requests.get(base_url, params=payload)
+        response_json = response.json()
+
+        comments_list = []
+        # print(len(comments_list) + 1)  # debug
+
+        if query_volume == 'once' or response_json.get('nextPageToken') is None:
+            comments_list += response_json['items']
+            store_csv(video_id, comments_list)
+            return comments_list
+
+        if query_volume == 'all':
+            # Getting all comments if there's a paging token, adding them all to the list after
+            while response_json.get('nextPageToken'):
+                next_page_token = response_json.get('nextPageToken')
+                payload = {
+                    'part': part,
+                    'maxResults': max_results,
+                    'order': order_by,
+                    'videoId': video_id,
+                    'pageToken': next_page_token,
+                    'key': api_key
+                }
+                response_json = requests.get(base_url, params=payload).json()
+                comments_list += response_json['items']
+                # print(len(comments_list))  # debug
+                store_csv(video_id, comments_list)
+
+
+def main():
+    """
+    Input Schema: inputs.csv
+    video_id, video_title
+    """
+
+    inputs = open('data/input/').readlines()
+
+    for video in inputs:
+        video_id_temp = video.split(",")[1].strip()
+        comments_list = send_request(resource='commentThreads',
+                                     query_volume='once',
+                                     video_id=video_id_temp,
+                                     part='snippet,replies',
+                                     max_results=100,
+                                     order_by='relevance')  # 'time' or 'relevance'
+        print(len(comments_list))
+        time.sleep(5)
+
+    return ''
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # parser.add_argument('user', help='Github username')
+    # parser.add_argument('repo', help='User\'s repo')
+    args = parser.parse_args()
+    # main(args.user, args.repo)
+    main()
+
 
 """
 * * Sample request:
@@ -98,119 +237,3 @@ part=snippet%2Creplies
   }, ...
 }
 """
-
-
-def timestamp_to_utc(timestamp):
-    """Convert unix timestamp to UTC date
-    :param timestamp: int - the unix timestamp integer
-    :return: utc_data - the date in YYYY-MM-DD format
-    """
-    timestamp = int(str(timestamp)[0:10])
-    utc_date = datetime.datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d')
-    return utc_date
-
-
-# TODO: revise and refactor
-def store_csv(video_id, json_response):
-    """Store the traffic stats as a CSV, with schema (refer to main()).
-
-    """
-    dates_and_views = OrderedDict()
-    detailed_views = json_response['views']
-    for row in detailed_views:
-        utc_date = timestamp_to_utc(int(row['timestamp']))
-        dates_and_views[utc_date] = (str(row['count']), str(row['uniques']))
-
-    # Starting up the CSV, writing the headers in a first pass
-    current_timestamp = str(datetime.datetime.now().strftime('%Y-%m-%d-%Hh-%Mm'))  # was .strftime('%Y-%m-%d'))
-    csv_file_name = 'data/' + current_timestamp + '-traffic-stats.csv'
-
-    for i in dates_and_views:
-        row = [repo_name, i, dates_and_views[i][0], dates_and_views[i][1]]
-        with open(csv_file_name, 'a') as csvfile:
-            csv_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            csv_writer.writerow(row)
-
-    return ''
-
-
-def send_request(resource, query_volume, video_id, part, max_results, order_by='relevance'):
-    """
-    :param resource: 'commentThreads' - only this for now
-    :param query_volume: 'all' or 'once' - set to 'all' to get all comments
-    :param video_id:
-    :param part:
-    :param max_results:
-    :param order_by:
-    :return:
-    """
-    if resource == 'commentThreads':
-        # GET https://www.googleapis.com/youtube/v3/commentThreads?
-        # ^ from https://developers.google.com/youtube/v3/guides/implementation/comments
-        base_url = 'https://www.googleapis.com/youtube/v3/commentThreads'
-        payload = {
-            'part': part,
-            'maxResults': max_results,
-            'order': order_by,
-            'videoId': video_id,
-            'key': api_key
-        }
-        response = requests.get(base_url, params=payload)
-        response_json = response.json()
-
-        comments_list = []
-        # print(len(comments_list) + 1)  # debug
-
-        if query_volume == 'once' or response_json.get('nextPageToken') is None:
-            comments_list += response_json['items']
-            return comments_list
-
-        if query_volume == 'all':
-            # Getting all comments if there's a paging token, adding them all to the list after
-            while response_json.get('nextPageToken'):
-                next_page_token = response_json.get('nextPageToken')
-                payload = {
-                    'part': part,
-                    'maxResults': max_results,
-                    'order': order_by,
-                    'videoId': video_id,
-                    'pageToken': next_page_token,
-                    'key': api_key
-                }
-                response_json = requests.get(base_url, params=payload).json()
-                comments_list += response_json['items']
-                # print(len(comments_list))  # debug
-
-
-def main():
-    """
-    Input Schema: inputs.csv
-    video_id, video_title
-
-    > Output Schema: [date_time]-comments.csv
-    video_id, comment_id, comment_date, updated_date, commenter_name, commenter_url, parent_comment, child_comment,
-
-    > Mock:
-    qwerty1234, asdf1234, 2016-09-09T11:52:18.000Z, 2016-09-09T11:52:18.000Z, randomUser, someURL, "
-
-    """
-    comments_list = send_request(resource='commentThreads',
-                                 query_volume='all',
-                                 video_id='srXsCRnSgBA',
-                                 part='snippet,replies',
-                                 max_results=100,
-                                 order_by='relevance')
-
-    print(len(comments_list))
-    # print(response_json)
-
-    return ''
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    # parser.add_argument('user', help='Github username')
-    # parser.add_argument('repo', help='User\'s repo')
-    args = parser.parse_args()
-    # main(args.user, args.repo)
-    main()
